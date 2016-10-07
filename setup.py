c2921cd835840d686a0d089b4a29651e1661a401
Iacchus Mercurius <kassivs@gmail.com>
2016-10-06 22:52:01 -0300

    Commit from make.sh at Thu Oct  6 22:52:01 BRT 2016

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
 
     #include_package_data = True,
     package_data={
-        '' :  ['*'],
+        '.' :  ['*'],
         'chimas' : ['requirements.txt','etc/*'],
     },
     #package_dir={'' : '.', 'chimas.core' : 'core/'},
